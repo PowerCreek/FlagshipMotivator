b5d14db570b9560f1f9546237d464f8b8bec7e76
Landan Parker <landanparker@algorist.io>
2022-12-10 18:14:00 -0600

    Created Markdown provider

---
+++ SoftwareClientFinder/components/Functional/MarkdownContent.tsx/MarkdownService.tsx
@@ -0,0 +1,26 @@
+export {}
+
+interface MarkupData{
+    tag: string,
+    text: string
+}
+
+const MarkupTags = {
+    bigLabel: (e:string)=> <h1>{e}</h1>,
+    bigLabel2: (e:string)=> <div className={styles.sloganText}>{e}</div>,
+}
+
+function GetComponentFromMarkup(data: MarkupData):JSX.Element{
+    return MarkupTags[data.tag as keyof typeof MarkupTags](data.text);
+}
+
+function MapMarkdownContent(markup: MarkupData[][]){
+    return markup.map(arr => 
+        (key: number)=>
+        <div key ={key}>
+            {arr.map(((e,i)=>
+                <div key={i}>{GetComponentFromMarkup(e)}</div>
+            ))}
+        </div>
+    )
+}
